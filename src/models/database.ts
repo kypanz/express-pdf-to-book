69c039a6e91cefc11e30c867661ff0b98eca7c8e
kypanz <kypanz@pop-os.localdomain>
2024-03-19 05:50:53 -0300

    Removing unecesary models interfaces and schemas

--- src/models/database.ts
+++ src/models/database.ts
@@ -14,36 +14,6 @@ export interface IUser {
     role: string;
 }
 
-export interface IChatSaved {
-    user_id: string;
-    thread_id: string;
-    initial_message: string;
-}
-
-export interface IDiary {
-    regular_user_id: string;
-    assigned_medic_id: string;
-    name_medic: string;
-    name_regular_user: string;
-    request_thread_id: string;
-    description_thread: string;
-    specialization_name: string;
-    date_scheduled: string;
-    meeting_url: string;
-    status: StatusDiary;
-}
-
-export interface ISpecialist {
-    user_id: string;
-    name: string;
-    tag_name: string;
-}
-
-export interface ISavedMessage {
-    user_id: string;
-    message_encrypted: string;
-}
-
 // Database Schemas
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
@@ -54,30 +24,6 @@ const userSchema = new Schema<IUser>({
     role: { type: String, required: true },
 }, { timestamps: true });
 
-const chatSavedSchema = new Schema<IChatSaved>({
-    user_id: { type: String, required: true },
-    thread_id: { type: String, required: true },
-    initial_message: { type: String, required: true }
-}, { timestamps: true });
-
-const scheduledSchema = new Schema<IDiary>({
-    regular_user_id: { type: String, required: true },
-    assigned_medic_id: { type: String, required: true },
-    name_medic: { type: String, required: true },
-    name_regular_user: { type: String, required: true },
-    request_thread_id: { type: String, required: true },
-    description_thread: { type: String, required: true },
-    specialization_name: { type: String, required: true },
-    date_scheduled: { type: String, required: false },
-    meeting_url: { type: String, required: false },
-    status: { type: Number, required: true }
-}, { timestamps: true });
-
-const specialistSchema = new Schema<ISpecialist>({
-    user_id: { type: String, required: true },
-    name: { type: String, required: true },
-    tag_name: { type: String, required: true },
-}, { timestamps: true });
 
 // Plugins
 userSchema.plugin(passportLocalMongoose);
