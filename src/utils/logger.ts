48c01ccb673b0c952526e046eb3bea8ad2cce3f9
kypanz <kypanz@pop-os.localdomain>
2024-04-30 23:28:21 -0300

    Adding winston

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+import winston from 'winston';
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+// Usage Example
+logger.log({ level: 'info', message: 'The message to save here' });
+
